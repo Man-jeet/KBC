698802d1f40da7eaa7332783929b2c8bee07cf9e
Manjeet <104201388+Man-jeet@users.noreply.github.com>
2022-12-29 09:48:41 +0530

    Update KBC.py

--- KBC.py
+++ KBC.py
@@ -4,7 +4,7 @@ if re=="yes":
     print("\twelcome to K.B.C game")
 am=0
 Qu =["\033[94m""1.Who is the first citizen of India","\033[94m""2.Name the planet known as the Red Planet?","\033[94m""3.How many seconds make one hour?","\033[94m""4.Which is the tallest mountain in the world?","\033[94m""5.Which continent is known as the Dark continent?"]
-op= ["\n1.pm\n2.cm\n3.precedent\n4.mla\n5.life line"],["\n1.earth\n2.mars\n3.moon\n4.vinus\n5.life line"],["\n1.3656\n2.3665\n3.3601\n4.3600\n5.life line"],["1.himaliya","2.maunt evrest","3.triund","4.rising star\n5.life line"],["1.south Africa","2.Africa","3.usa","4.uae\n5.life line"]
+op= ["\n1.Prime Minister\n2.Chief Minister\n3.President\n4.Mla\n5.life line"],["\n1.earth\n2.mars\n3.moon\n4.vinus\n5.life line"],["\n1.3656\n2.3665\n3.3601\n4.3600\n5.life line"],["1.himaliya","2.maunt evrest","3.triund","4.rising star\n5.life line"],["1.south Africa","2.Africa","3.usa","4.uae\n5.life line"]
 sol=[3,2,4,2,2,]
 life_line=["1.50_50","2.audiance poll","3.phone a friend","4.skip question"]
 for i in range(len(Qu)):
